023e0591432f493b469dea54d08ee029a68c92ce
Leak <sethpowellbusiness@gmail.com>
2021-08-23 11:41:45 -0700

    imported DayOfWeek comp into calendar-wrapper.js

--- src/components/calendar-wrapper.js
+++ src/components/calendar-wrapper.js
@@ -1,9 +1,17 @@
 import React from "react";
 
+import DayOfWeek from "./day-of-week"
+
 export default function calendarWrapper(props) {
     return (
         <div className="calendar-wrapper">
-            Calendar goes here
+            <DayOfWeek day="Sunday" />
+            <DayOfWeek day="Monday" />
+            <DayOfWeek day="Tuesday" />
+            <DayOfWeek day="Wednesday" />
+            <DayOfWeek day="Thursday" />
+            <DayOfWeek day="Friday" />
+            <DayOfWeek day="Saturday" />
         </div>
     )
 }
