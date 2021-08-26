c86fd60665421b38859afaaf151ea001824055d9
Leak <sethpowellbusiness@gmail.com>
2021-08-26 11:01:40 -0700

    forgot to save on last commit

--- src/components/calendar-box.js
+++ src/components/calendar-box.js
@@ -1,11 +1,69 @@
 import React, { Component } from "react";
 
 export default class CalendarBox extends Component {
+    constructor() {
+        super();
+
+        this.state={
+            reminderExists: false,
+            textInput: ""
+        }
+
+        this.handleReminderChange = this.handleReminderChange.bind(this)
+    }
+
+    handleReminderChange() {
+        if (!this.state.reminderExists && this.state.textInput !== "") {
+            // post request id, month, date
+            fetch("http://127.0.0.1:5000/reminder/add", {
+                method: "POST",
+                headers: {"content-type": "application/json"},
+                body: JSON.stringify({
+                    text: this.state.textInput,
+                    date: this.props.date,
+                    month_id: this.props.month.id
+                })
+            })
+            .then(response => response.json())
+            .then(data => {
+                if (typeof data === "string") {
+                    console.log("Error in reminder post request: ", data)
+                }
+                else {
+                    this.setState({ reminderExists: true })
+                }
+            })
+            .catch(error => console.log("Error in reminder post request: ", error))
+        }
+        else if (this.state.reminderExists && this.state.textInput !== "") {
+            //put request
+            fetch(`http://127.0.0.1:5000/reminder/update/${this.props.month.id}/${this.props.date}`, {
+                method: "PUT",
+                headers: {"content-type": "application/json"},
+                body: JSON.stringify({text: this.state.textInput})
+            })
+            .then(response => response.json())
+            .then(data => {
+                if (typeof data === "string") {
+                    console.log("Error in reminder put request: ", data)
+                }
+            })
+            .catch(error => console.log("Error in reminder put request: ", error))
+        }
+        else if (this.state.reminderExists && this.state.textInput === "") {
+            // delete request
+            fetch(`http://127.0.0.1:5000/reminder/delete/${this.props.month.id}/${this.props.date}`, {method: "DELETE"})
+            .then(response => response.json())
+            .then(data => {this.setState({ reminderExists: false })})
+            .catch(error => console.log("Error deleting reminder: ", error))
+        }
+    }
+
     render() {
         return (
             <div className="calendar-box">
                 <span>{this.props.date}</span>
-                <textarea></textarea>
+                <textarea onBlur={this.handleReminderChange} onChange={(event => this.setState({ textInput: event.target.value })).bind(this)}></textarea>
             </div>
         )
     }
